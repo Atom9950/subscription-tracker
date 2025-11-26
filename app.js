bb62a8edc78af610a0afd9d7344c28db8ac411ca
Sayandeep <sdroy001@gmail.com>
2025-11-26 20:25:55 +0530

    Added authRoutes to the system

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 import express from "express";
 import { PORT } from "./config/env.js";
+import subscriptionRouter from "./routes/subscription.routes.js";
+import userRouter from "./routes/user.routes.js";
+import authRouter from "./routes/auth.routes.js";
 
 const app = express();
+
+app.use("/api/v1/subscription", subscriptionRouter);
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/auth", authRouter);
 
 app.get("/", (req, res) => {
   res.send("Welcome to Onboarding!");
